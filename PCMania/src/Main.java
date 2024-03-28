32f48350354213e7b6f2e246dcbedd6de5b0cd55
Júlio César <56365230+juliocloud@users.noreply.github.com>
2024-03-28 11:12:47 -0300

    docs: comments

--- PCMania/src/Main.java
+++ PCMania/src/Main.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-    public int OPCAO_FINALIZAR = 0;
-    public int PC_INDISPONIVEL = -1;
 
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
+
+        // Evitar conflito com a opcao 0 (sair)
         int[] opcoesDisponiveisDePc = {1, 2, 3};
 
         Computador[] PCS = new Computador[3];
@@ -54,12 +54,16 @@ public class Main {
         cliente.cpf = input.nextLong();
 
         do {
+            // Acabou Dinheiro sempre true, ate que se prove o contrario
             acabouDinheiro = true;
+
             System.out.println("Qual opção de PC você gostaria de comprar?");
             for (int i = 0; i < opcoesDisponiveisDePc.length; i++) {
                 System.out.print(opcoesDisponiveisDePc[i] + " ");
             }
+
             System.out.println("(Selecione 0 para sair)");
+
             opcao = input.nextInt();
 
             if(opcao != 0) {
@@ -67,21 +71,16 @@ public class Main {
                     if (cliente.computadores[i] == null) {
                         cliente.computadores[i] = PCS[opcao - 1];
                         acabouDinheiro = false;
+                        // Se ainda tem slot de PC disponivel, pode preencher
                         break;
                     }
                 }
                 System.out.println("PC adicionado ao carrinho: ");
                 PCS[opcao - 1].mostraPCConfigs();
             }
-
         } while(opcao != 0 && !acabouDinheiro);
-
         System.out.println("Total da compra: ");
         System.out.println("R$" + cliente.calculaTotalCompra());
-
     }
-
-
-
 
 }
